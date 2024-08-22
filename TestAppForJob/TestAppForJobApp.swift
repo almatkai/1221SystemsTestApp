42f297d75cfda80afaf4796d150620394649ca45
Almat Kairatov <92521753+almatkai@users.noreply.github.com>
2024-08-22 16:56:34 +0500

    Initial Commit

---
+++ TestAppForJob/TestAppForJobApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppForJobApp.swift
+//  TestAppForJob
+//
+//  Created by Almat Kairatov on 22.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAppForJobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
